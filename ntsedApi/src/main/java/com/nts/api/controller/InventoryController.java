3826b0f607030b8d123bd58fdd363ae22ef670bc
SanHao666 <141192784+SanHao666@users.noreply.github.com>
2023-09-08 17:11:46 +0900

    初回

---
+++ ntsedApi/src/main/java/com/nts/api/controller/InventoryController.java
@@ -0,0 +1,30 @@
+package com.nts.api.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/inventory")
+public class InventoryController {
+
+    @GetMapping("/form")
+    public String showInventoryForm(Model model) {
+       
+        return "inventoryPage";
+    }
+
+    @PostMapping("/submit")
+    public String submitInventoryForm(
+        @RequestParam("stockName") String stockName,
+        @RequestParam("unitSelect") String unitSelect,
+        @RequestParam("remarks") String remarks,
+        Model model
+    ) {
+        
+        return "redirect:/inventory/form";
+    }
+}
