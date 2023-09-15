c2bed66e1c1973862c00ef7c90f3957ff44302d1
Sandi <82432916+SuMyat-Sandi@users.noreply.github.com>
2023-09-15 15:27:03 +0900

    update

--- ntsedApi/src/main/java/com/nts/api/controller/StockIoFormController.java
+++ ntsedApi/src/main/java/com/nts/api/controller/StockIoFormController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,12 +24,12 @@ public class StockIoFormController {
 	@Autowired
 	private StockIoFormService stockIoFormService;
 	
-	
+	private StockIo existingStock;
 	@GetMapping("/{id}/stockioregister")
 	public String stockioForm(@PathVariable String id,Model model) {
 		
 		
-		StockIo existingStock= stockIoFormService.getStocInfokById(id);
+		existingStock= stockIoFormService.getStocInfokById(id);
 		model.addAttribute("stockIoForm", existingStock);
 		
 		List<InOutType> inOutType = stockIoFormService.getInOutType();
@@ -38,10 +39,14 @@ public class StockIoFormController {
 	}
 	
 	@PostMapping("/submit")
-	public String createStockio(@Valid StockIo stockIo,BindingResult bindingResult,
-			@RequestParam("button") String button) {
+	public String createStockio(@Validated StockIo stockIo,BindingResult bindingResult,
+			@RequestParam("button") String button,Model model) {
+		
+		model.addAttribute("stockIoForm", existingStock);
+		
 		if ("登録".equals(button)) {
 			if(bindingResult.hasErrors()) {
+				System.out.print(stockIo.getIoNum());
 				return "stockIoForm";
 			}
 	
--- ntsedApi/src/main/java/com/nts/api/entity/StockIo.java
+++ ntsedApi/src/main/java/com/nts/api/entity/StockIo.java
@@ -2,6 +2,7 @@ package com.nts.api.entity;
 
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
@@ -14,11 +15,9 @@ public class StockIo {
 	private int stockNum;
 	
 	@NotNull(message = " ※ 入出庫タイプを入力してください")
-	@Min(value=1,message = " ※ 入出庫タイプを入力してください")
 	private Integer ioTypeId;
 	
 	@NotNull(message = " ※ 入出庫数を入力してください")
-	@Min(value=1, message = " ※ ")
 	private Integer ioNum;
 	
 	private String remarks;
